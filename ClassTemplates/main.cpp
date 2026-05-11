f549e1459c0bee2c7cb55527fd524062c47d5b25
StudioAlbert <account@studio-albert.com>
2026-05-11 16:18:40 +0200

    Building multiple executable project

---
+++ ClassTemplates/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+// Template class
+template<typename Tcoord>
+struct Point {
+    Tcoord x;
+    Tcoord y;
+
+    const Tcoord operator+(const Tcoord& other) const{
+        return {x + other.x, y + other.y};
+    }
+
+    static float distance(Point a, Point b){
+        return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y,2));
+    }
+
+};
+
+
+int main(){
+
+    Point<int> int_point{1,2};
+    Point<float> A{1.0f, 2.0f};
+    Point<float> B{3.0f, 4.0f};
+
+    Point<float>::distance(A, B);
+
+
+}
---
+++ FunctionTemplates/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+
+template<typename  T>
+T add(T a, T b){
+    return a + b;
+}
+
+int main(){
+
+    auto result_numeric = add<int>( 1, 1);
+    std::cout << "Result ? " << result_numeric << std::endl;
+    return 0;
+
+}
