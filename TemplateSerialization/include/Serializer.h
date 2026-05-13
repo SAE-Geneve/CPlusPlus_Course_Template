e313ef95551e5bd329dea9f7325feeca31662cec
StudioAlbert <account@studio-albert.com>
2026-05-13 07:17:07 +0200

    Add TemplateSerialization module with serializer implementation

---
+++ TemplateSerialization/include/Serializer.h
@@ -0,0 +1,32 @@
+﻿//
+// Created by sebas on 13.05.2026.
+//
+
+#ifndef CPLUSPLUS_COURSE_TEMPLATE_SERIALIZER_H
+#define CPLUSPLUS_COURSE_TEMPLATE_SERIALIZER_H
+#include <string>
+
+// Primary template — general case
+template <typename T>
+class Serializer {
+public:
+  static std::string serialize(T val) { return std::to_string(val); }
+};
+
+// Full specialization for bool
+template <>
+class Serializer<bool> {
+public:
+  static std::string serialize(bool val) { return val ? "true" : "false"; }
+};
+
+// Partial specialization — matches any pointer
+template <typename T>
+class Serializer<T*> {
+public:
+  static std::string serialize(T* ptr) {
+    return ptr ? Serializer<T>::serialize(*ptr) : "null";
+  }
+};
+
+#endif // CPLUSPLUS_COURSE_TEMPLATE_SERIALIZER_H
---
+++ TemplateSerialization/main.cc
@@ -0,0 +1,12 @@
+﻿#include "include/Serializer.h"
+
+#include <iostream>
+#include <ostream>
+//
+// Created by sebas on 13.05.2026.
+//
+int main() {
+  Serializer<int> serializer;
+  std::cout << serializer.serialize(10) << std::endl;
+
+}
